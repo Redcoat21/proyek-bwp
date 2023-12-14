99708fd84134e851707009acd57b21dd12cf269b
Dahren Ericsson <89411491+Redcoat21@users.noreply.github.com.>
2023-12-14 22:41:04 +0700

    Get top lecturers should be working correctly.

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -24,14 +24,20 @@ class PageController extends Controller
 
     public function showHome(): Application | Factory| \Illuminate\Contracts\View\View| \Illuminate\Foundation\Application
     {
-        return view('home');
+        return view('home', ['topLecturers' => $this->getTopThreeLecturer()]);
     }
 
     public function getTopThreeLecturer(): JsonResponse
     {
-        $res = DB::select("SELECT course, COUNT(*) AS `occurences` FROM transactions GROUP BY course LIMIT 3");
+        $res = DB::select("
+            SELECT u.username, u.name, u.profile_picture
+            FROM transactions AS `t`
+            LEFT JOIN courses AS `c` ON c.id = t.course
+            LEFT JOIN lecturers AS `l` ON l.username = c.lecturer
+            LEFT JOIN users AS `u` ON u.username = l.username
+            GROUP BY u.username, u.name, u.profile_picture LIMIT 3
+        ");
         return response()->json($res);
-//        dd($res);
     }
 
     public function toggleLogin()
