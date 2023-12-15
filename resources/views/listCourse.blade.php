f375777f93941fd641429567ebb17bb916243fb9
RedFrosy <112476864+RedFrosy@users.noreply.github.com>
2023-12-15 22:16:26 +0700

    fixing search false position

--- resources/views/listCourse.blade.php
+++ resources/views/listCourse.blade.php
@@ -28,13 +28,13 @@ Courses
             </div>
             <div class="flex">
                 <div class="relative block">
-                    <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                    <div class="absolute inset-y-0 start-0 flex items-center py-4 ms-2 pointer-events-none">
                     <svg class="w-4 h-4 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
                     </svg>
                     <span class="sr-only">Search icon</span>
                     </div>
-                    <input type="text" id="search-navbar" class="block p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500" placeholder="Search...">
+                    <input type="text" id="search-navbar" class="block p-2 my-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500" placeholder="Search...">
                 </div>
                 <div class="relative block my-2 mx-3">
                     @if (!auth()->user())
