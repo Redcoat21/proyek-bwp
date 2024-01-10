25e129aafb8587ff159e4ddedbb40f2716e57366
hansp17 <130662485+hansp17@users.noreply.github.com>
2024-01-10 21:01:25 +0700

    mengambil data course dan menyelesaikan addCourse

--- resources/views/lecturer/addCourse.blade.php
+++ resources/views/lecturer/addCourse.blade.php
@@ -7,7 +7,7 @@ Add Course
 @section('header')
 <nav class="bg-white">
     <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-1 p-4">
-        <a href="{{ route('lecturerProfile.get') }}" class="flex items-center space-x-3 text-black">
+        <a href="{{ route('profile.get') }}" class="flex items-center space-x-3 text-black">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
             </svg>
@@ -23,34 +23,35 @@ Add Course
 </div>
 <h1 class="block text-center text-2xl text-blue-400 my-3">Add Course</h1>
 <div class="px-96">
-    <form action="{{ route('auth.post') }}" method="post" class="col-span-2 col-start-3 mt-2">
+    <form action="" method="post" class="col-span-2 col-start-3 mt-2">
         @csrf
         <input type="hidden" name="type" value="login">
 
         <div class="col-span-2 col-start-3 mt-3">
             <label for="" class="block text-sm font-medium leading-6 text-gray-900">COURSE TITLE <span class="text-red-600">*</span></label>
             <div class="mt-1">
-                <input id="" name="" type="text" class="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 text-sm leading-6" placeholder="Course Name">
+                <input id="" name="title" type="text" class="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 text-sm leading-6" placeholder="Course Name">
             </div>
         </div>
         <div class="col-span-2 col-start-3 mt-3">
             <label for="" class="block text-sm font-medium leading-6 text-gray-900">PRICE <span class="text-red-600">*</span></label>
             <div class="mt-1">
-                <input id="" name="" type="text" class="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 text-sm leading-6" placeholder="Rp. 100.000,-">
+                <input id="" name="price" type="text" class="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 text-sm leading-6" placeholder="100000 (*harus angka)">
             </div>
         </div>
         <div class="col-span-2 col-start-3 mt-3">
             <label for="" class="block text-sm font-medium leading-6 text-gray-900">DESCRIPTION <span class="text-red-600">*</span></label>
             <div class="mt-1">
-            <textarea id="textInput" name="text" rows="4" cols="50"  class="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 text-sm leading-6" placeholder="Description"></textarea>
+            <textarea id="textInput" name="desc" rows="4" cols="50"  class="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 text-sm leading-6" placeholder="Description"></textarea>
             </div>
         </div>
-        <div class="col-span-2 col-start-3 mt-3">
+        {{-- tidak ada course text --}}
+        {{-- <div class="col-span-2 col-start-3 mt-3">
             <label for="" class="block text-sm font-medium leading-6 text-gray-900">COURSE TEXT <span class="text-red-600">*</span></label>
             <div class="mt-1">
             <textarea id="textInput" name="text" rows="6" cols="50"  class="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 text-sm leading-6" placeholder="Course"></textarea>
             </div>
-        </div>
+        </div> --}}
         <div class="col-span-2 col-start-3 mt-3">
             <label for="" class="block text-sm font-medium leading-6 text-gray-900">COVER <span class="text-red-600">*</span></label>
             <div class="mt-1">
